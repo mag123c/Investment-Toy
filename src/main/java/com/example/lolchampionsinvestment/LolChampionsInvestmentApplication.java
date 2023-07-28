129a818fe98afb36311c2fb8cbfb17ba23dd31c0
장재호 <diehreo@gmail.com>
2023-07-28 23:28:40 +0900

    [Project Create]

---
+++ src/main/java/com/example/lolchampionsinvestment/LolChampionsInvestmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.lolchampionsinvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolChampionsInvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolChampionsInvestmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lolchampionsinvestment/LolChampionsInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lolchampionsinvestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolChampionsInvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
